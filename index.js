df930a6f41299602260f082767f827e99e495b77
sabrina <sabrinadjebbar93@gmail.com>
2022-03-24 14:37:21 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+function returnsANamedFunction(){
+  return function name(){};
+  
+}
+
+
+function returnsAnAnonymousFunction(){
+    return function(){};
+}
